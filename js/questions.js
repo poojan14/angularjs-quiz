802bbf5e486d849a027cc5877364a21cf7ee2ac4
poojan14 <pvs09747@gmail.com>
2022-07-14 23:53:52 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,96 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is a Model in MVC",
+    answer: "for maintaining data",
+    options: [
+      "for displaying data",
+      "for manipulating data",
+      "for maintaining data",
+      "for sending data to server"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which directive is used to start an AngularJS application",
+    answer: "ng-app",
+    options: [
+      "ng-if",
+      "ng-model",
+      "ng-start",
+      "ng-app"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which AngularJS component responsible for setting up route definitions?",
+    answer: "$routeProvider",
+    options: [
+      "$routeProvider",
+      "$route",
+      "$routeSet",
+      "$routeParams"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the below is not a AngularJS validation directive?",
+    answer: "ng-length",
+    options: [
+      "ng-pattern",
+      "ng-length",
+      "ng-messages",
+      "ng-required"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Due to which concept there is automatic synchronization of data between model and view components?",
+    answer: "Data Binding",
+    options: [
+      "MVC supported",
+      "Frontend framework",
+      "Data Binding",
+      "Directives"
+    ]
+  },
+
+ {
+    numb: 6,
+     question: "$scope is a glue between",
+     answer: "view and controller",
+     options: [
+       "model and controller",
+       "view and controller",
+       "view and model",
+       "All of the above"
+     ]
+   },
+
+  
+     {
+     numb: 7,
+     question: "Which directive is used to remove element?",
+     answer: "ng-if",
+     options: [
+       "ng-if",
+       "ng-remove",
+       "ng-bind",
+       "ng-include"
+     ]
+   },
+
+  
+    {
+     numb: 8,
+     question: "Which is of the following is incorrect Filter?",
+     answer: "bold",
+     options: [
+       "orderBy",
+       "limitTo",
+       "bold",
+       "currency"
+     ]
+   },
+];
